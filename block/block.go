2812d43ba60fee55395194cebabafd4cf8d510de
Adam Grimm <atgrimm@gmail.com>
2019-12-04 15:53:43 -0800

    refactor, gofmt

--- block/block.go
+++ block/block.go
@@ -1,20 +1,20 @@
 package block
 
 import (
-	"errors"
 	"crypto"
 	"crypto/rsa"
 	"encoding/json"
+	"errors"
 )
 
 type block struct {
 	header header
-	body body
+	body   body
 }
 
 type header struct {
-	nonce string
-	hash string
+	nonce   string
+	hash    string
 	prevBlk string
 }
 
@@ -23,46 +23,70 @@ type body struct {
 }
 
 type transaction struct {
-	sender *rsa.PublicKey
+	sender    *rsa.PublicKey
 	receivers unspentCoins
-	amount int
+	amount    int
 	signature []byte
 }
 
 type unspentCoins map[string]int
 
 func ValidateTransaction(txn transaction, unspent unspentCoins) error {
+	err := validateAmount(txn)
+	if err != nil {
+		return err
+	}
+	err = validateHoldings(txn, unspent)
+	if err != nil {
+		return err
+	}
+	err = validateSig(txn)
+	if err != nil {
+		return err
+
+	}
+	// txn is valid
+	return nil
+}
+
+func validateAmount(txn transaction) error {
 	// validate amount
 	totalOut := 0
 	for _, amount := range txn.receivers {
-		totalOut = totalOut + amount 
+		totalOut = totalOut + amount
 	}
 	if txn.amount != totalOut {
 		return errors.New("amount in does not equal amount out")
 	}
+	return nil
+}
+
+func validateHoldings(txn transaction, unspent unspentCoins) error {
 	// validate sender has enough coins
 	senderName, _ := json.Marshal(txn.sender)
 	holding, ok := unspent[string(senderName)]
 	if !ok || holding < txn.amount {
 		return errors.New("sender does not have enough coins")
 	}
+	return nil
+}
+
+func validateSig(txn transaction) error {
 	// validate signature
-	
 	msgObj := transaction{
-		sender: txn.sender,
+		sender:    txn.sender,
 		receivers: txn.receivers,
-		amount: txn.amount,
+		amount:    txn.amount,
 	}
-	msg, _  := json.Marshal(msgObj)
+	msg, _ := json.Marshal(msgObj)
 	newHash := crypto.SHA256
 	pssh := newHash.New()
 	pssh.Write(msg)
 	hashed := pssh.Sum(nil)
 
-	err := rsa.VerifyPKCS1v15(txn.sender, newHash, hashed, txn.signature); if err != nil {
+	err := rsa.VerifyPKCS1v15(txn.sender, newHash, hashed, txn.signature)
+	if err != nil {
 		return errors.New("invalid sig")
 	}
-	// txn is valid
 	return nil
 }
-	
--- block/block_test.go
+++ block/block_test.go
@@ -1,13 +1,13 @@
 package block
 
 import (
-	"testing"
-	"crypto/rsa"
 	"crypto"
 	"crypto/rand"
-	"errors"
+	"crypto/rsa"
 	"encoding/json"
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestValidateTransaction(t *testing.T) {
@@ -15,87 +15,87 @@ func TestValidateTransaction(t *testing.T) {
 	pubKey := &pvKey.PublicKey
 	name, _ := json.Marshal(pubKey)
 	nameStr := string(name)
-	tests := []struct{
-		desc string
-		txn transaction
-		us unspentCoins
+	tests := []struct {
+		desc       string
+		txn        transaction
+		us         unspentCoins
 		invalidSig bool
-		err error
+		err        error
 	}{
 		{
 			desc: "Happy case",
-			txn : transaction{
+			txn: transaction{
 				sender: pubKey,
-		receivers: unspentCoins{
-				"p1": 20,
-				"p2": 30,
+				receivers: unspentCoins{
+					"p1": 20,
+					"p2": 30,
+				},
+				amount: 50,
 			},
-		amount: 50,
-	},
-	us: unspentCoins{nameStr:100},
-	err: nil,
-},
-{
-	desc: "amount in does not equal amount out",
-	txn : transaction{
-		sender: pubKey,
-		receivers: unspentCoins{
-			"p1": 20,
-			"p2": 30,
+			us:  unspentCoins{nameStr: 100},
+			err: nil,
 		},
-		amount: 60,
-	},
-	us: unspentCoins{nameStr: 100},
-	err: errors.New("amount in does not equal amount out"),
-},
-{
-	desc: "sender doesn't have enough coins",
-	txn : transaction{
-		sender: pubKey,
-		receivers: unspentCoins{
-			"p1": 20,
-			"p2": 30,
+		{
+			desc: "amount in does not equal amount out",
+			txn: transaction{
+				sender: pubKey,
+				receivers: unspentCoins{
+					"p1": 20,
+					"p2": 30,
+				},
+				amount: 60,
+			},
+			us:  unspentCoins{nameStr: 100},
+			err: errors.New("amount in does not equal amount out"),
 		},
-		amount: 60,
-	},
-	us: unspentCoins{nameStr: 10},
-	err: errors.New("not enough coins"),
-},
-{
-	desc: "invalid sig",
-	txn : transaction{
-		sender: pubKey,
-		receivers: unspentCoins{
-			"p1": 20,
-			"p2": 30,
+		{
+			desc: "sender doesn't have enough coins",
+			txn: transaction{
+				sender: pubKey,
+				receivers: unspentCoins{
+					"p1": 20,
+					"p2": 30,
+				},
+				amount: 60,
+			},
+			us:  unspentCoins{nameStr: 10},
+			err: errors.New("not enough coins"),
 		},
-		amount: 50,
-	},
-	us: unspentCoins{nameStr: 100},
-	invalidSig: true,
-	err: errors.New("invalid sig"),
-},
-}
-for _, tt := range tests {
-	newHash := crypto.SHA256
-	msg, _ := json.Marshal(tt.txn)
-	pssh := newHash.New()
-	if tt.invalidSig {
-		msg = append(msg, []byte("incorrect")...)
+		{
+			desc: "invalid sig",
+			txn: transaction{
+				sender: pubKey,
+				receivers: unspentCoins{
+					"p1": 20,
+					"p2": 30,
+				},
+				amount: 50,
+			},
+			us:         unspentCoins{nameStr: 100},
+			invalidSig: true,
+			err:        errors.New("invalid sig"),
+		},
 	}
-	pssh.Write(msg)
-	hashed := pssh.Sum(nil)
+	for _, tt := range tests {
+		newHash := crypto.SHA256
+		msg, _ := json.Marshal(tt.txn)
+		pssh := newHash.New()
+		if tt.invalidSig {
+			msg = append(msg, []byte("incorrect")...)
+		}
+		pssh.Write(msg)
+		hashed := pssh.Sum(nil)
 
-	sig, err := rsa.SignPKCS1v15(rand.Reader, pvKey, crypto.SHA256, hashed)
-	if err != nil {
-		assert.Fail(t, "test faild with err", err)
+		sig, err := rsa.SignPKCS1v15(rand.Reader, pvKey, crypto.SHA256, hashed)
+		if err != nil {
+			assert.Fail(t, "test faild with err", err)
+		}
+		tt.txn.signature = sig
+		err = ValidateTransaction(tt.txn, tt.us)
+		if tt.err != nil {
+			assert.Error(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
 	}
-	tt.txn.signature = sig
-	err = ValidateTransaction(tt.txn, tt.us) 
-	if tt.err != nil {
-		assert.Error(t, err)
-	} else {
-		assert.Nil(t, err)
-	}
-}
 }
